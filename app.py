8ee3b79f633e8252c7326bc2ac06455cac42cce6
rnfhana <130295481+rnfhana@users.noreply.github.com>
2023-12-03 02:56:29 +0000

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from sqlalchemy import text
 
 css_link = '<link rel="stylesheet" href="styles.css">'
+st.markdown(css_link, unsafe_allow_html=True)
 
 list_programs = ['', 'General English', 'Intensive IELTS', 'TOEFL Preparation', 'Business English', 'Conversational English']
 list_gender = ['', 'male', 'female']
