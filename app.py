38dbfedc88f7554ea2d50ca09cc1c17abb6536c0
rnfhana <130295481+rnfhana@users.noreply.github.com>
2023-12-05 06:32:56 +0000

    update

--- app.py
+++ app.py
@@ -99,3 +99,7 @@ if page == "Visualisasi Data":
     st.subheader("Visualisasi Programs")
     data = conn.query('SELECT programs, COUNT(*) as count FROM participant GROUP BY programs;')
     st.bar_chart(data.set_index('programs'))
+
+    st.subheader("Visualisasi Duration")
+    data = conn.query('SELECT duration, COUNT(*) as count FROM participant GROUP BY duration;')
+    st.bar_chart(data.set_index('duration'))
